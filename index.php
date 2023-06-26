48dc0280f56ebde5cd8bb409419211eddf427c3d
Stephanie Nelima <35595163+Stenewa@users.noreply.github.com>
2023-06-26 11:28:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+<h1><?php bloginfo('name')?></h1>
+<p><?php bloginfo('description')?></p>
+<?php
+ while (have_posts()) {
+ 	the_post();?>
+ 	<h2><a href="<?php the_permalink()?>"><?php the_title();?></a></h2>
+
+ }
+?>
+	
---
+++ single.php
@@ -0,0 +1,10 @@
+<h1><?php bloginfo('name')?></h1>
+<p><?php bloginfo('description')?></p>
+<?php
+ while (have_posts()) {
+ 	the_post();?>
+ 	<h2><a href="<?php the_permalink()?>"><?php the_title();?></a></h2>
+ 	
+ }
+?>
+	
